01b0ae4c6a4feb0ad53aabb7f82bd1d9ab53c28b
Arm0ny <63424081+Arm0ny@users.noreply.github.com>
2023-10-30 09:24:53 +0100

    front-end modifications

--- front/src/app/components/enter-email/enter-email.component.ts
+++ front/src/app/components/enter-email/enter-email.component.ts
@@ -37,4 +37,21 @@ export class EnterEmailComponent implements OnInit{
         );
     }
   }
+
+  openAppStore() {
+    const userAgent = navigator.userAgent;
+    const isIOS = /iPad|iPhone|iPod/.test(userAgent);
+    const isAndroid = /Android/.test(userAgent);
+
+    if (isIOS) {
+      // Apri App Store su iOS
+      window.open('https://apps.apple.com/it/app/meetravel/id1532228470');
+    } else if (isAndroid) {
+      // Apri Google Play Store su Android
+      window.open('https://play.google.com/store/apps/details?id=com.meetravel&gl=IT');
+    } else {
+      // Codice per browser web o altri dispositivi
+      window.open('https://meetravel.it/entra-in-app/', '_blank');
+    }
+  }
 }
